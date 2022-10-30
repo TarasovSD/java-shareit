026229c18f3a95170da5f5ebb8c820ca32a021d2
Sergey Tarasov <s.d.tarasov@ya.ru>
2022-10-30 12:20:20 +0300

    fix: checkstyle

--- src/main/java/ru/practicum/shareit/exceptions/EndBeforeStartException.java
+++ src/main/java/ru/practicum/shareit/exceptions/EndBeforeStartException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.exceptions;
 
-public class EndBeforeStartException extends RuntimeException{
+public class EndBeforeStartException extends RuntimeException {
     public EndBeforeStartException(String message) {
         super(message);
     }
